7f4efdeca13c6be80c7c0c4d63703cc66212756c
A-1104 <avi01techify@gmail.com>
2024-06-18 11:37:30 +0530

    changed the url

--- src/frontend/src/dataservices.js
+++ src/frontend/src/dataservices.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 
 
-export const url = "http://52.29.9.150:80"
+export const url = "http://54.167.157.145:80"
 
 export const listPupils = writable([]);
 export const selectedPupil = writable(); 
